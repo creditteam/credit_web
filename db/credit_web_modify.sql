77182cb94ab8cd5045cc0f959473605683bbef24
huixiong <huixiong@juanpi.com>
2016-07-20 09:48:57 +0800

    1:数据库表结构变更更新

--- db/credit_web_modify.sql
+++ db/credit_web_modify.sql
@@ -1,6 +1,9 @@
+/** cr_credit table change*/
 alter table cr_credit modify disposal_type varchar(16);
 alter table cr_credit modify debt_proof varchar(256);
-/***/
+alter table cr_credit add COLUMN credit_type smallint(3)COMMENT '债权类型(1债权处置，2债权转让)';
+/** cr_reward table change*/
 alter table cr_reward modify images varchar(256);
-
+/** cr_file_manager table change*/
 alter table cr_file_manager change dowload_url download_url varchar(128);
+
